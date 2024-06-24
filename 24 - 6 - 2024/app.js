a184be9fd81c698d739c6dbc8301b548ee6d6d7a
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-25 01:04:01 +0500

    TS and JS files added

--- 24 - 6 - 2024/app.js
+++ 24 - 6 - 2024/app.js
@@ -1,1 +1,9 @@
 "use strict";
+let num = 12;
+num = 13;
+let str = "this is string";
+str = "also string";
+function add(userName) {
+    console.log(userName);
+}
+add("Syed Uzair");
---
+++ 24 - 6 - 2024/app.ts
@@ -0,0 +1,8 @@
+let num = 12
+num = 13
+let str:string= "this is string"
+str = "also string"
+function add(userName:string):void{
+    console.log(userName);
+}
+add("Syed Uzair")
