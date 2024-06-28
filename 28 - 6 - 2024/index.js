43b1a7bf3baa6cefb4a7f9d718b3a6743815491c
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-28 15:58:33 +0500

    all practiced

--- 28 - 6 - 2024/index.js
+++ 28 - 6 - 2024/index.js
@@ -4,4 +4,19 @@ let user = "hello";
 user = 123;
 // user = true // this is wrong
 console.log(user);
-// inference
+// inference means making types automatic //implicitly
+let id = 12344;
+id = 443234;
+let car = {
+    cName: "mehran",
+    model: 2000,
+    average: (fuel, km) => {
+        console.log(fuel / km);
+    }
+};
+car.average(20, 500);
+car.variant = 2004;
+let dog = {
+    limbs: 4,
+    isBark: true
+};
--- 28 - 6 - 2024/index.ts
+++ 28 - 6 - 2024/index.ts
@@ -3,5 +3,43 @@ let user: string|number= "hello";
 user = 123;
 // user = true // this is wrong
 console.log(user);
-// inference
+// inference means making types automatic //implicitly
+let id = 12344;
+id = 443234;
+
+// adding types in object
+// and optional by adding "?"
+// adding type in function
+
+type Car = {
+    cName : string, 
+    model : number,
+    variant? :number,
+    average :(fuel:number,km:number)=>void
+}
+let car :Car ={
+    cName: "mehran",
+    model : 2000,
+    average : (fuel,km) => {
+        console.log(fuel/km);
+    } 
+}
+car.average(20 ,500)
+
+car.variant = 2004;
+
+
+
+// interface can be extended
+// no need of "="
+interface Animal {
+    limbs : number,   
+}
+interface Dog extends Animal{
+    isBark : boolean
+}
+let dog: Dog = {
+    limbs : 4,
+    isBark: true
+}
 
