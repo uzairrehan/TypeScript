8a5613a5d1447fe66ae2c8fa20ce46f0e377d519
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-28 15:13:33 +0500

    all ok

---
+++ 28 - 6 - 2024/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// union type
+let user = "hello";
+user = 123;
+// user = true // this is wrong
+console.log(user);
+// inference
---
+++ 28 - 6 - 2024/index.ts
@@ -0,0 +1,8 @@
+// union type
+let user: string|number= "hello";
+user = 123;
+// user = true // this is wrong
+console.log(user);
+
+// inference
+
