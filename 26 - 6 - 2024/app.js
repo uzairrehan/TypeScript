3819885ae15617159cc46a0d7027a44ee088eb6a
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-06-28 14:05:46 +0500

    all ok

--- 26 - 6 - 2024/app.js
+++ 26 - 6 - 2024/app.js
@@ -1,1 +1,8 @@
 "use strict";
+let array = ["uzair", 23];
+let userName = "taha";
+console.log(userName);
+function newUser(name1) {
+    console.log(name1);
+}
+newUser("Hello World");
---
+++ 26 - 6 - 2024/app.ts
@@ -0,0 +1,16 @@
+type array1 = (string|number)[]
+
+let array = ["uzair", 23]
+
+
+type user = string
+let userName:user = "taha"
+
+console.log(userName)
+  
+function newUser(name1:string):void {
+    console.log(name1)
+    
+}
+newUser("Hello World")
+
