f93c8674cbbb5b6f928c867bc03d5e5112b04d06
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-07-01 15:20:48 +0500

    practiced

---
+++ 1 -  7 - 2024/app.ts
@@ -0,0 +1,69 @@
+// type Cat = {
+//     limbs: number,
+//     alive:boolean,
+//     speed:(fuel:number,distance:number)=>number
+// }
+// let cat:Cat = {
+//     limbs:12,
+//     alive:true,
+//     speed:(fuel,distance)=>{
+//         return fuel / distance 
+//     }
+// }
+// console.log(cat.speed(40,20));
+
+
+
+
+
+
+
+
+
+// Tuples
+// let basic : readonly [str.ing,string,string]= ["red", "yellow", "blue"]
+
+// basic.push("black")
+// let basicTuples :  readonly[string ,number , boolean] = ['Name' , 19, true]
+// basicTuples.push('??')
+
+// enum OrderStatus{
+//     PENDING,
+//     CONFIRMED,
+//     SHIPPED,
+//     DELIVERED
+// }
+// console.log(OrderStatus.SHIPPED)
+
+
+
+
+
+// enum HttpStatus{
+//     success = 200,
+//     yourMistake = 400,
+//     myMistake = 500
+// }
+// console.log(HttpStatus.myMistake)
+// console.log(HttpStatus[400])
+
+
+// enum Random{
+//     ramdomNum = Math.random()
+// }
+// console.log(Random.ramdomNum)
+
+
+
+
+
+// enum Province {
+//     sindh = "sindh",
+//     kp = "Kpk",
+//     bal = "Balochistan"
+// }
+// console.log(Province.kp);
+// console.log(Province.bal);
+// console.log(Province.sindh);
+
+
